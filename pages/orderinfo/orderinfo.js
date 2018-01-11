4ad57992c00ef94de24e78274732a1a9933d5621
yeren0914 <yeren0914@qq.com>
2018-01-11 21:46:10 +0800

    orderinfo

--- pages/orderinfo/orderinfo.js
+++ pages/orderinfo/orderinfo.js
@@ -7,14 +7,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    orders:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    const ids = options.id;
+    console.log(ids);
   },
 
   /**
